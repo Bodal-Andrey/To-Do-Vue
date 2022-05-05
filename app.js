773b2d08528d53ac4036ac81bf5551f126cf592e
Bodal-Andrey <a-v-bodal@yandex.ru>
2022-05-05 18:27:44 +0500

    Добавит основной функционал

--- app.js
+++ app.js
@@ -1,22 +1,56 @@
 new Vue({
     el: '#app',
     data: {
+        currentTask: '',
+        editValue: '',
         tasks: [
             {
                 text: 'Subscribe to channel',
                 isCompleted: false,
+                isEditing: false,
             },
             {
                 text: 'Like the video',
                 isCompleted: false,
+                isEditing: false,
             },
             {
                 text: 'Learn Vue.js',
                 isCompleted: true,
+                isEditing: false,
             },
         ],
     },
     methods: {
-
+        addTask() {
+            this.tasks.push({
+                text: this.currentTask,
+                isCompleted: false,
+            });
+            this.currentTask = '';
+        },
+        removeTask(taskText) {
+            this.tasks = this.tasks.filter(task => {
+                return task.text !== taskText;
+            });
+        },
+        changeEditing(taskText) {
+            this.editValue = taskText;
+            this.tasks = this.tasks.map(task => {
+                if (task.text === taskText) {
+                    task.isEditing = !task.isEditing;
+                }
+                return task;
+            });
+        },
+        editTask(taskText) {
+            this.tasks = this.tasks.map(task => {
+                if (task.text === taskText) {
+                    task.isEditing = !task.isEditing;
+                    task.text = this.editValue;
+                }
+                return task;
+            });
+        }
     },
 });
