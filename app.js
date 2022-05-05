5a5e7c971346e17c3e161d4736756e4fcaf1ac00
Bodal-Andrey <a-v-bodal@yandex.ru>
2022-05-05 17:33:29 +0500

    Создаст стартовую структуру

---
+++ app.js
@@ -0,0 +1,22 @@
+new Vue({
+    el: '#app',
+    data: {
+        tasks: [
+            {
+                text: 'Subscribe to channel',
+                isCompleted: false,
+            },
+            {
+                text: 'Like the video',
+                isCompleted: false,
+            },
+            {
+                text: 'Learn Vue.js',
+                isCompleted: true,
+            },
+        ],
+    },
+    methods: {
+
+    },
+});
